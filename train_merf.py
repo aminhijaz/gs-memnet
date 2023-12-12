9125e8d7550001c7f62f9f90d00f3a9dfdfe832b
aminhijaz <aminhijaz2001@gmail.com>
2023-12-12 01:27:11 -0500

    hi

--- train_merf.py
+++ train_merf.py
@@ -37,15 +37,14 @@ def train_merf(dataset : ModelParams, iteration : int, pipeline : PipelineParams
     merf_eval_model = ResMem(pretrained=True)
     merf = MerfNet(gaussians, render_merf, single_camera[0], merf_eval_model, pipeline, background)
 
-    optimizer = torch.optim.AdamW(merf.parameters(), lr=0.01)
-
+    optimizer = torch.optim.Adam([self.camera_pos], lr=learning_rate)
     prev_loss = 0
     loop = tqdm(range(1000))
     for i in loop:
         optimizer.zero_grad()
         loss, _ = merf()
         loss.backward(retain_graph=True)
-        print(loss)
+        print(merf.camera_pos.grad)
         optimizer.step()
 
         # if i % 100 == 0:
