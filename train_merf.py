5604b21afddd07e26627937186f5eb84baccb3e8
aminhijaz <aminhijaz2001@gmail.com>
2023-12-12 01:54:47 -0500

    hi

--- train_merf.py
+++ train_merf.py
@@ -40,7 +40,8 @@ def train_merf(dataset : ModelParams, iteration : int, pipeline : PipelineParams
 
     optimizer = torch.optim.Adam([merf.camera_pos], lr=0.1)
     prev_loss = 0
-    for i in 1000:
+    loop = range(1000)
+    for i in loop:
         optimizer.zero_grad()
         loss, _ = merf()
         loss.backward(retain_graph=True)
