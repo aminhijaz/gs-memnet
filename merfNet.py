47643c50fd1ebd3f5dea911a01ff3db6acd7c436
aminhijaz <aminhijaz2001@gmail.com>
2023-12-12 01:23:01 -0500

    hi

--- merfNet.py
+++ merfNet.py
@@ -66,10 +66,7 @@ class MerfNet(nn.Module):
         Rt_bottom = torch.tensor([[0., 0., 0., 1.]], device="cuda")
         Rt_top_right = T.view(3, 1)  # Reshape T to [3, 1] if it's not already
         Rt = torch.cat([torch.cat([Rt_top_left, Rt_top_right], dim=1), Rt_bottom], dim=0)
-        Rt.requires_grad = True
-
         # Now set requires_grad to True
-        Rt.requires_grad = True
         pc = self.gaussians
         self.raster_settings = GaussianRasterizationSettings(
         image_height=int(self.camera.image_height),
