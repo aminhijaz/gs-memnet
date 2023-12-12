3b602f983a892c3cb3c84935b22ed1e5c363b823
aminhijaz <aminhijaz2001@gmail.com>
2023-12-12 01:02:24 -0500

    hi

--- merfNet.py
+++ merfNet.py
@@ -5,10 +5,11 @@ import torch.nn.functional as F
 import torchvision
 from torchvision import transforms
 from scene.cameras import Camera as GSCamera
-
-
+from diff_gaussian_rasterization import GaussianRasterizationSettings, GaussianRasterizer
+from resmem import ResMem
+import math
 class MerfNet(nn.Module):
-    def __init__(self, gaussians, renderer, camera, resmodel, loss_fn=torch.nn.MSELoss(), device="cuda"):
+    def __init__(self, gaussians, renderer, camera, resmodel, pipeline, background, loss_fn=torch.nn.MSELoss(), device="cuda"):
         super().__init__()
         self.device = device
         self.gaussians = gaussians
@@ -18,53 +19,150 @@ class MerfNet(nn.Module):
             # transforms.CenterCrop(227),
         ))
         self.camera = camera
+        self.background = background
+        self.pipeline = pipeline
         # optimize the camera translation
-        self.camera_pos = nn.Parameter(torch.from_numpy(np.array(camera.T, dtype=np.float32)).to(device))
+        print(self.camera.camera_center)
+        self.camera_pos = nn.Parameter(
+            torch.from_numpy(np.array([3.0,  6.9, +2.5], dtype=np.float32)).to(device))
         self.resmodel = resmodel.to(device)
         for param in self.resmodel.parameters():
             param.requires_grad = False
         self.resmodel.eval()
         self.loss_fn = loss_fn
+        tanfovx = math.tan(self.camera.FoVx * 0.5)
+        tanfovy = math.tan(self.camera.FoVy * 0.5)
+        self.raster_settings = GaussianRasterizationSettings(
+        image_height=int(self.camera.image_height),
+        image_width=int(self.camera.image_width),
+        tanfovx=tanfovx,
+        tanfovy=tanfovy,
+        bg=self.background,
+        scale_modifier=1.0,
+        viewmatrix=self.camera.world_view_transform,
+        projmatrix=self.camera.full_proj_transform,
+        sh_degree=self.gaussians.active_sh_degree,
+        campos=self.camera_pos,
+        prefiltered=False,
+        debug=self.pipeline.debug
+    )
+        self.rasterizer = GaussianRasterizer(raster_settings=self.raster_settings)
+        self.rasterizer.eval()
+        for param in self.rasterizer.parameters():
+            param.requires_grad = False
+
+
+
+
+
 
     def forward(self):
-        self.update_camera()
-        image = self.renderer(self.camera, self.gaussians)
-        i = image
-        # i = image[0, :, :, :3]
-        # i = i.permute(2, 0, 1)
-        i = self.transform(i.unsqueeze(0))
-        prediction = self.resmodel.forward(i.to(self.device))
-        loss = self.loss_fn(prediction, torch.ones(1, 1).to(self.device))
-
-        return loss, image, prediction
-
-    def update_camera(self):
+        # R = look_at_rotation(self.camera_pos[None, :], device=self.device)
+        # T = -torch.bmm(R.transpose(1, 2), self.camera_pos[None, :, None])[:, :, 0]
+        # R = R.squeeze(0)
+        tanfovx = math.tan(self.camera.FoVx * 0.5)
+        tanfovy = math.tan(self.camera.FoVy * 0.5)
         R = look_at_rotation(self.camera_pos[None, :], device=self.device)
         T = -torch.bmm(R.transpose(1, 2), self.camera_pos[None, :, None])[:, :, 0]
         R = R.squeeze(0)
-        new_R = R.clone().detach().cpu().numpy()
-        new_T = T.clone().detach().cpu().numpy()
-        prev_camera = self.camera
-        new_camera = GSCamera(
-            prev_camera.colmap_id, new_R, new_T,
-            prev_camera.FoVx, prev_camera.FoVy,
-            prev_camera.original_image, None,
-            prev_camera.image_name, prev_camera.uid
-        )
-        self.camera = new_camera
+        Rt = torch.zeros(4,4, device="cuda")
+        Rt[:3, :3] = R.transpose(0,1)
+        Rt[:3, 3] = T
+        Rt[3, 3] = 1.0
 
+        pc = self.gaussians
+        self.raster_settings = GaussianRasterizationSettings(
+        image_height=int(self.camera.image_height),
+        image_width=int(self.camera.image_width),
+        tanfovx=tanfovx,
+        tanfovy=tanfovy,
+        bg=self.background,
+        scale_modifier=1.0,
+        viewmatrix=T,
+        projmatrix=self.camera.full_proj_transform,
+        sh_degree=self.gaussians.active_sh_degree,
+        campos=self.camera_pos,
+        prefiltered=False,
+        debug=self.pipeline.debug
+    )
+        self.rasterizer.raster_settings = self.raster_settings
+        screenspace_points = torch.zeros_like(pc.get_xyz, dtype=pc.get_xyz.dtype, requires_grad=True, device="cuda") + 0
+        try:
+            screenspace_points.retain_grad()
+        except:
+            pass
+
+        means3D = pc.get_xyz
+        means2D = screenspace_points
+        opacity = pc.get_opacity
+
+    # If precomputed 3d covariance is provided, use it. If not, then it will be computed from
+    # scaling / rotation by the rasterizer.
+        scales = None
+        rotations = None
+        cov3D_precomp = None
+        if self.pipeline.compute_cov3D_python:
+            cov3D_precomp = self.gaussians.get_covariance(3)
+        else:
+            scales = self.gaussians.get_scaling
+            rotations = self.gaussians.get_rotation
+
+    # If precomputed colors are provided, use them. Otherwise, if it is desired to precompute colors
+    # from SHs in Python, do it. If not, then SH -> RGB conversion will be done by rasterizer.
+        shs = None
+        colors_precomp = None
+        shs = self.gaussians.get_features
+
+        rendered_image, _ = self.rasterizer(
+            means3D = means3D,
+            means2D = means2D,
+            shs = shs,
+            colors_precomp = colors_precomp,
+            opacities = opacity,
+            scales = scales,
+            rotations = rotations,
+            cov3D_precomp = cov3D_precomp)
+        i = rendered_image
+        # i = image[0, :, :, :3]
+        # i = i.permute(2, 0, 1)
+        i = self.transform(i.unsqueeze(0))
+        prediction = self.resmodel.forward(i)
+        loss = self.loss_fn(prediction, torch.ones(1, 1).to(self.device))
+        return loss, rendered_image
 
 def look_at_rotation(
-    camera_position, 
-    at=((0, 0, 0),), 
-    up=((0, 1, 0),),
-    device="cuda"):
+    camera_position, at=((0, 0, 0),), up=((0, 1, 0),), device = "cpu"
+) -> torch.Tensor:
+    """
+    This function takes a vector 'camera_position' which specifies the location
+    of the camera in world coordinates and two vectors `at` and `up` which
+    indicate the position of the object and the up directions of the world
+    coordinate system respectively. The object is assumed to be centered at
+    the origin.
 
+    The output is a rotation matrix representing the transformation
+    from world coordinates -> view coordinates.
+
+    Args:
+        camera_position: position of the camera in world coordinates
+        at: position of the object in world coordinates
+        up: vector specifying the up direction in the world coordinate frame.
+
+    The inputs camera_position, at and up can each be a
+        - 3 element tuple/list
+        - torch tensor of shape (1, 3)
+        - torch tensor of shape (N, 3)
+
+    The vectors are broadcast against each other so they all have shape (N, 3).
+
+    Returns:
+        R: (N, 3, 3) batched rotation matrices
+    """
+    # Format input and broadcast
     broadcasted_args = convert_to_tensors_and_broadcast(
         camera_position, at, up, device=device
     )
     camera_position, at, up = broadcasted_args
-
     for t, n in zip([camera_position, at, up], ["camera_position", "at", "up"]):
         if t.shape[-1] != 3:
             msg = "Expected arg %s to have shape (N, 3); got %r"
@@ -79,7 +177,6 @@ def look_at_rotation(
         replacement = F.normalize(torch.cross(y_axis, z_axis, dim=1), eps=1e-5)
         x_axis = torch.where(is_close, replacement, x_axis)
     R = torch.cat((x_axis[:, None, :], y_axis[:, None, :], z_axis[:, None, :]), dim=1)
-    
     return R.transpose(1, 2)
 
 def convert_to_tensors_and_broadcast(
