e12be784e893d5da6386857d51c7f2ebbe251483
aminhijaz <aminhijaz2001@gmail.com>
2023-12-13 04:53:27 -0500

    HI

--- diff_gaussian_rasterization/diff_gaussian_rasterization/__init__.py
+++ diff_gaussian_rasterization/diff_gaussian_rasterization/__init__.py
@@ -58,9 +58,6 @@ class _RasterizeGaussians(torch.autograd.Function):
         view_matrix,
         raster_settings,
     ):
-        print(view_matrix.requires_grad)
-        print(view_matrix.requires_grad)
-        print(view_matrix.requires_grad)
 
         # Restructure arguments the way that the C++ lib expects them
         args = (
@@ -110,7 +107,8 @@ class _RasterizeGaussians(torch.autograd.Function):
         num_rendered = ctx.num_rendered
         raster_settings = ctx.raster_settings
         colors_precomp, means3D, scales, rotations, cov3Ds_precomp, radii, sh, geomBuffer, binningBuffer, imgBuffer, alpha, view_matrix = ctx.saved_tensors
-        print(view_matrix.requires_grad)
+        print("view:")
+        print(view_matrix)
 
         # Restructure args as C++ method expects them
         args = (raster_settings.bg,
@@ -149,7 +147,6 @@ class _RasterizeGaussians(torch.autograd.Function):
                 raise ex
         else:
              grad_means2D, grad_colors_precomp, grad_opacities, grad_means3D, grad_cov3Ds_precomp, grad_sh, grad_scales, grad_rotations, grad_viewmatrix = _C.rasterize_gaussians_backward(*args)
-        print(grad_viewmatrix)
         grads = (
             grad_means3D,
             grad_means2D,
@@ -162,6 +159,7 @@ class _RasterizeGaussians(torch.autograd.Function):
             grad_viewmatrix,
             None,
         )
+        print(grad_viewmatrix)
 
         return grads
 
